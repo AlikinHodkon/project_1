1b151665cd17810000a6fc04ba65004145c403da
Alikin Hodkon <kakto228030904@gmail.com>
2024-07-08 10:37:15 +0300

    predeploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://AlikinHodkon.github.io/project_1",
+  base: "https://alikinhodkon.github.io/project_1",
   plugins: [react()],
 })
